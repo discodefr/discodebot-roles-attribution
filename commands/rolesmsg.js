d5aa4254df5e2bf2ce50cc25bc65f60f073a6b40
discodefr <discodefr.contact@gmail.com>
2018-12-09 20:34:47 +0100

    Lol

--- commands/rolesmsg.js
+++ commands/rolesmsg.js
@@ -7,6 +7,7 @@ exports.run = (client, message, args) => {
     if (!ownersids.includes(message.author.id)) return;
 
     //emojis
+    var girorougeem = message.guild.emojis.find(em => em.id === "521399571188809728")
     var othersem = message.guild.emojis.find(em => em.id === "482579389335339009")
     var djsem = message.guild.emojis.find(em => em.id === "482460553256894477")
     var pyem = message.guild.emojis.find(em => em.id === "482461186210660372")
@@ -20,7 +21,7 @@ exports.run = (client, message, args) => {
     var bashem = message.guild.emojis.find(em => em.id === "482540545156186112")
     var rubyem = message.guild.emojis.find(em => em.id === "482535746788524043")
     var javaem = message.guild.emojis.find(em => em.id === "482538218378756106")
-    var phpem = message.guild.emojis.find(em => em.id === "482533506996764682")
+    var phpem = message.guild.emojis.find(em => em.id === "521398414328463365")
     var luaem = message.guild.emojis.find(em => em.id === "521324768910376961")
 
     var bdoem = message.guild.emojis.find(em => em.id === "516981489397923860")
@@ -42,8 +43,8 @@ exports.run = (client, message, args) => {
     var rolesembed = new Discord.RichEmbed()
         .setColor("186BBE")
         .setAuthor("DisCode®", message.guild.iconURL)
-        .addField("Vos pratiques", ":video_game: : Gameur\n\n:open_file_folder: : Développeur\n\n:desktop: : Informaticien\n\n:pencil2: : Graphiste\n\n:saxophone: : Musicien\n", true)
-        .addField("Notifications", ":one: : Notif ANNONCES\n\n:two: : Notif PARTENARIATS\n\n:three: : Notif EVENTS\n\n:four: : Notif UPDATES\n\n:five: : Notif SONDAGES\n", true)
+        .addField("Vos pratiques", ":video_game: • <@&482551543338500137>\n\n:open_file_folder: • <@&482464432039919626>\n\n:desktop: • <@&503872954593968128>\n\n:pencil2: • <@&482468587005542401>\n\n:saxophone: • <@&482474709729148938>\n", true)
+        .addField("Notifications", ":one: • <@&482571748794171392>\n\n:two: • <@&482571876133109761>\n\n:three: • <@&482571969158447105>\n\n:four: • <@&482572024896684042>\n\n:five: • <@&498055718457573378>\n", true)
         .addField("_ _", "_ _")
         .addField("\n\n" + othersem + " HypeSquad et Nitro", "Veuillez contacter un administrateur pour avoir accès aux rôles HypeSquad et Nitro.")
         .setTimestamp(new Date)
@@ -62,8 +63,8 @@ exports.run = (client, message, args) => {
                                                 var rolesembed1 = new Discord.RichEmbed()
                                                     .setColor("186BBE")
                                                     .setAuthor("DisCode®", message.guild.iconURL)
-                                                    .addField("Rôles développeurs", djsem + " : Discord.js\n\n" + pyem + " : Python\n\n" + htmlem + " : HTML\n\n" + cssem + " : CSS\n\n" + jsem + " : JavaScript\n\n" + lcem + " : Langage C\n\n" + cppem + " : C++\n", true)
-                                                    .addField("_ _", csem + " : C#\n\n" + goem + " : Go\n\n" + bashem + " : Bash\n\n" + rubyem + " : Ruby\n\n" + javaem + " : Java\n\n" + phpem + " : PhP\n\n" + luaem + " : Lua", true)
+                                                    .addField("Rôles développeurs", djsem + " • <@&482522792152268811>\n\n" + pyem + " • <@&482517526220242954>\n\n" + htmlem + " • <@&482526265966854145>\n\n" + cssem + " • <@&482526332350234645>\n\n" + jsem + " • <@&482517589298511891>\n\n" + lcem + " • <@&482518807965990912>\n\n" + cppem + " • <@&482530444659785758>\n", true)
+                                                    .addField("_ _", csem + " • <@&482530510065631253>\n\n" + goem + " • <@&482530768833478656>\n\n" + bashem + " • <@&482531108043358208>\n\n" + rubyem + " • <@&482530845693968384>\n\n" + javaem + " • <@&482530959267201054>\n\n" + phpem + " • <@&482531325786587147>\n\n" + luaem + " • <@&521375041242529792>", true)
                                                     .setTimestamp(new Date)
                                                     .setFooter("DisCode®", client.user.avatarURL)
                                                 message.channel.send(rolesembed1).then(rolesembed1 => {
@@ -84,8 +85,8 @@ exports.run = (client, message, args) => {
                                                                                                             var rolesembed2 = new Discord.RichEmbed()
                                                                                                                 .setColor("186BBE")
                                                                                                                 .setAuthor("DisCode®", message.guild.iconURL)
-                                                                                                                .addField("Rôles gameurs", bdoem + " : Black Desert Online\n\n" + codem + " : Call of Duty 4\n\n" + owem + " : Overwatch\n\n" + desem + " : Destiny 2\n\n" + ftnem + " : Fortnite\n\n" + spltem + " : Splatoon\n\n" + csgoem + " : CS:GO", true)
-                                                                                                                .addField("_ _", rblem + " : Roblox\n\n" + csem + " : CS:GO\n\n" + forzaem + " : Forza\n\n" + falloutem + " : Fallout\n\n" + mcem + " : Minecraft\n\n" + pldem + " : Paladins", true)
+                                                                                                                .addField("Rôles gameurs", bdoem + " • <@&516975473780195336>\n\n" + codem + " • <@&516975472110993410>\n\n" + owem + " • <@&516975473776001044>\n\n" + desem + " • <@&516975471075000360>\n\n" + ftnem + " • <@&516976808264990779>\n\n" + spltem + " • <@&516975470731067392>\n\n" + csgoem + " • <@&518169169519181831>", true)
+                                                                                                                .addField("_ _", rblem + " • <@&516975472471441440>\n\n" + forzaem + " • <@&518169168965795840>\n\n" + falloutem + " • <@&518169191489208323>\n\n" + mcem + " • <@&518169380597661743>\n\n" + pldem + " • <@&518742072497537034>", true)
                                                                                                                 .setTimestamp(new Date)
                                                                                                                 .setFooter("DisCode®", client.user.avatarURL)
                                                                                                             message.channel.send(rolesembed2).then(rolesembed2 => {
@@ -94,14 +95,14 @@ exports.run = (client, message, args) => {
                                                                                                                         rolesembed2.react(owem).then(() => {
                                                                                                                             rolesembed2.react(desem).then(() => {
                                                                                                                                 rolesembed2.react(ftnem).then(() => {
-                                                                                                                                    rolesembed2.react(csgoem).then(()=> {
-                                                                                                                                        rolesembed2.react(spltem).then(() => {
+                                                                                                                                    rolesembed2.react(spltem).then(()=> {
+                                                                                                                                        rolesembed2.react(csgoem).then(() => {
                                                                                                                                             rolesembed2.react(rblem).then(() => {
-                                                                                                                                                rolesembed2.react(csem).then(() => {
-                                                                                                                                                    rolesembed2.react(forzaem).then(() => {
-                                                                                                                                                        rolesembed2.react(falloutem).then(() => {
-                                                                                                                                                            rolesembed2.react(mcem).then(() => {
-                                                                                                                                                                rolesembed2.react(pldem)
+                                                                                                                                                rolesembed2.react(forzaem).then(() => {
+                                                                                                                                                    rolesembed2.react(falloutem).then(() => {
+                                                                                                                                                        rolesembed2.react(mcem).then(() => {
+                                                                                                                                                            rolesembed2.react(pldem).then(()=> {
+                                                                                                                                                                message.channel.send("_ _\n" + girorougeem + " **Pour rappel, plusieurs messages-rôles sont disponibles.** " + girorougeem + "\n\n1 • Les rôles concernant votre **pratique**, les **notifications**.\n2 • Les rôles de **langages de programmation** pour les **développeurs**.\n3 • Les rôles **jeux-vidéos** pour les **gameurs**.")
                                                                                                                                                             })
                                                                                                                                                         })
                                                                                                                                                     })
--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,6 +1,7 @@
 const messagesids = ["521381162849927168", "521381181095280650", "521381204382187530"]
+const Discord = require('discord.js')
 
-exports.run = (client, member, channel) => {
+exports.run = (client, reaction, member) => {
 
-    channel.fetchMessage('521386267716616192')
+    
 }
